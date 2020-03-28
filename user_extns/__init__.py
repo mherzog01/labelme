34059268f4618393e53bf83a89c5d8a8a695a5e5
mherzog01 <mherzog01@gmail.com>
2020-03-27 23:40:25 -0400

    Daily save

--- user_extns/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-
-from .tools import exportByLot
-from .tools import launchExternalViewer
-from .tools import imgFileToLabelFileName
-from .tools import create_connection
-from .tools import dispMsgBox
-from .tools import inputdialog
